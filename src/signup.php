66b5e270c7f338fe273bcf5613ed3ce4e155c22f
andres <uno@unicesmag.edu.co>
2025-04-03 20:37:24 -0500

    validate email exist

--- src/signup.php
+++ src/signup.php
@@ -5,17 +5,34 @@ $lname = $_POST['l_name'];
 $email = $_POST['e_mail'];
 $passw = $_POST['p_assw'];
 
-$sql = "INSERT INTO users
-(firstname,lastname,email,password)
-VALUES ('$fname','$lname','$email','$passw')
-";
-$ans = pg_query($conn, $sql);
-if($ans){
+$sql_validate_email="select count(id) as total
+ from 
+ users
+ where
+  email= '$email' and 
+   status = true
+;";
+$ans = pg_query($conn,$sql_validate_email);
+if($ans){//$ans == true
+    $row = pg_fetch_assoc($ans);
+    if ($row['total']>0){
+        echo"users already exists!!";
+    }
+ else{
+
+  $sql = "INSERT INTO users
+  (firstname,lastname,email,password)
+ VALUES ('$fname','$lname','$email','$passw')
+ ";
+ $ans = pg_query($conn, $sql);
+ if($ans){
     echo"user has been created successfully";
-}else{
+ }else{
     echo "error";
+  }
+  }
+ }else{
+     echo"query error";
 }
 
-
-
-?>
+ ?>
