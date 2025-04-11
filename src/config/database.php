838e147a685830a729307afdc17a4ebfb329f287
andres <uno@unicesmag.edu.co>
2025-04-10 21:50:10 -0500

    validar

--- src/config/database.php
+++ src/config/database.php
@@ -1,10 +1,10 @@
 <?php 
 
-$host = "localhost";
-$port ="5432";
-$dbname= "petstore";
-$user = "postgres";
-$password="unicesmag";
+$host = "aws-0-us-east-1.pooler.supabase.com";
+$port = "6543";
+$dbname = "postgres";
+$user = "postgres.ngislruakgwcoqagnxwb";
+$password = "unicesmag@@";
 
 $data_connection="
    host=$host
@@ -12,7 +12,7 @@ $data_connection="
    dbname=$dbname
    user=$user
    password=$password
-   ";
+";
 
 $conn = pg_connect($data_connection);
 if(!$conn){
